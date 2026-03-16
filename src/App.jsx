3072163aacc9dfc44f20372b23d0a52ec7bc3f9e
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-03-17 00:01:01 +0600

    Show flags, and handle data error

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 
+
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './components/countries/Countries'
+
+
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all').then(res => res.json())
+
+// console.log(countriesPromise);
+
 
 function App() {
   
 
   return (
     <>
-        
-        <Countries></Countries>
+        <Suspense fallback={<h4>Jhank is coming soon..</h4>}>
+
+        <Countries countriesPromise={countriesPromise}></Countries>
+        </Suspense>
      
 
      
